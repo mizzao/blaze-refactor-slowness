d776b50f359f3ab9113d7283be41f426d8e6cafd
Andrew Mao <mizzao@gmail.com>
2014-11-22 02:17:43 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Demo of how refactoring of Blaze slowed down UI performance significantly.
+
+Run the app with
+
+```
+meteor --release 0.8.2
+```
+
+then run it with
+
+```
+meteor --release 0.8.3
+```
+
+to see the difference.
---
+++ blaze-refactor-slowness.js
@@ -0,0 +1,80 @@
+Stuff = new Meteor.Collection("stuff");
+Letters = new Meteor.Collection("letters");
+
+if (Meteor.isClient) {
+  Deps.autorun(function() {
+     if (Session.equals("render-stuff", true))
+       Meteor.subscribe("stuff");
+  });
+
+  Template.hello.helpers({
+    renderStuff: function() {
+      return Session.equals("render-stuff", true);
+    },
+    stuff: function () {
+      return Stuff.find();
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // template data, if any, is available in 'this'
+      Session.set("render-stuff", !Session.get("render-stuff"));
+    }
+  });
+
+  Template.tlookup.helpers({
+    letter: function() { return Letters.findOne({key: ""+this}); }
+  });
+
+  function nl2br(str) {
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2');
+  }
+
+  Template.descriptionList.helpers({
+    properties: function() {
+      var result = [];
+      for (var key in this) {
+        var value = this[key];
+        result.push({
+          key: key,
+          value: value
+        });
+      }
+      return result;
+    },
+    value: function() {
+      if (this.value === false) return "false";
+      else if (_.isObject(this.value)) return JSON.stringify(this.value);
+      else return nl2br(this.value);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("stuff", function() {
+    return [ Stuff.find(), Letters.find() ];
+  });
+
+  Meteor.startup(function () {
+    if (Letters.find().count() === 0) {
+      for( var i = 0; i < 26; i++ ) {
+        var o = {};
+        o.key = String.fromCharCode[i];
+        o.value = Random.id();
+        Letters.insert(o);
+      }
+    }
+    if (Stuff.find().count() === 0) {
+      for( var i = 0; i < 200; i++ ) {
+        Stuff.insert({
+          a: i,
+          b: "some text for B",
+          c: "some text for C",
+          d: "some text for D",
+          e: "some multi-line\nwrapped text"
+        });
+      }
+    }
+  });
+}
